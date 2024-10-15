cf5b8acf2969c790b5ef7e65edfdff2784df393b
Tharunekaa Madhavan <156326384+tharunekaamadhavan@users.noreply.github.com>
2024-10-15 14:52:00 +0530

    first upload

---
+++ avg.py
@@ -0,0 +1,9 @@
+a=int(input('Enter Physics marks'))
+b=int(input('Enter Chemistry marks'))
+c=int(input('Enter Mathematics marks'))
+d=int(input('Enter Python marks'))
+e=int(input('Enter English marks'))
+f=a+b+c+d+e
+print('Total marks=',f)
+g=f/5
+print('Average=',g)
